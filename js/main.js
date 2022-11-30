2c501f3d7dab93e195a59b4775e47f54acf88c5d
SOHEE <118507151+devheee@users.noreply.github.com>
2022-11-30 16:18:03 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
+$(function () {
 
-$('.mainSlide').slick({
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    pauseOnHover: false,
-    pauseOnFocus: false,
+    $('.mainSlide').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
 
-})
+    $('.subSlide').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
 
-$('.subSlide').slick({
-    dots: false,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    pauseOnHover: false,
-    pauseOnFocus: false,
+    $('.subSlide').on('afterChange', function (e, s, c) {
+        $('.subVV .sub_num').text(c + 1 + "/" + s.slideCount);
+    });
+
 })
