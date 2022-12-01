1196f01da5bba07383691bdf220c296c18710e6a
SOHEE <118507151+devheee@users.noreply.github.com>
2022-12-01 17:03:48 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,6 +10,23 @@ $(function () {
         autoplaySpeed: 4000,
         pauseOnHover: false,
         pauseOnFocus: false,
+
+        customPaging: function (slider, i) {
+            var thumb = $(slider.$slides[i]).data();
+            if (i == '0') {
+                i = "기업시민";
+            } else if (i == '1') {
+                i = "스마트컨스트럭션";
+            } else if (i == '2') {
+                i = "탄소중립경영";
+            } else if (i == '3') {
+                i = "더샵";
+            } else if (i == '4') {
+                i = "도시정비";
+            } else if (i == '5') {
+                i = "기업문화";
+            } return '<a class="dot">' + i + '</a>';
+        },
     });
 
     $('.subSlide').slick({
